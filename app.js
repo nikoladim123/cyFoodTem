82fc14a92ed05b202d90391480c86429c309c1e4
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-04 18:09:43 +0100

    two more anims added

--- app.js
+++ app.js
@@ -66,12 +66,39 @@ threePicDivClass[2].addEventListener('mouseover',function(){
     },1500)
   }
 });
-// cheap animation change backgroundColor
+// cheap animation Bolero grill and bar
 var main = document.getElementsByClassName('mainMain')[0];
 var yellowBoxBar = document.getElementsByClassName('yellowBoxBar')[0];
+
+var getinTouchImageBox = document.getElementsByClassName('getinTouchImageBox')[0];
+var getInTouchYellowBox = document.getElementsByClassName('getInTouchYellowBox')[0];
+
 window.onscroll = function(){
     if(main.getBoundingClientRect().top - window.innerHeight <= 0){
-      yellowBoxBar.style.backgroundColor = '#f0cd3c'
-        console.log("TRIGGER: top of div reached.");
+      yellowBoxBar.style.backgroundColor = '#f0cd3c';
     }
+    //cheap animation contact
+    if(getinTouchImageBox.getBoundingClientRect().top - (window.innerHeight/1.5)<= 0){
+      getInTouchYellowBox.style.backgroundColor = 'rgb(217,185,86)';
+    }
+    threeGlasAnim();
+    specialsAnim();
+}
+//3 pic glass animation
+var barPicOne = document.getElementsByClassName('barPicOne')[0];
+var barPicThree = document.getElementsByClassName('barPicThree')[0];
+function threeGlasAnim(){
+  if(barPicOne.getBoundingClientRect().top - (window.innerHeight/1.5)<= 0){
+    barPicOne.style.left = '0';
+    barPicThree.style.right = '0';
+  }
+}
+//specials reveal anim specials connectWithUsParagraph
+var specials = document.getElementsByClassName('specials')[0];
+var connectWithUsParagraph = document.getElementsByClassName('connectWithUsParagraph')[0];
+function specialsAnim(){
+  if(specials.getBoundingClientRect().top - (window.innerHeight/1.5)<= 0){
+    specials.style.backgroundPosition = '0 0';
+    connectWithUsParagraph.style.top = '0';
+  }
 }
