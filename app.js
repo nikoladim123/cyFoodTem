1a224bb8f2aaf83609dc1e87dc3ec671ec78fd9a
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-04 23:08:37 +0100

    sticky menu added

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 //burgerMenu
-var burgerDiv = document.getElementsByClassName('burgerDiv')[0];
+var burgerDiv = document.getElementsByClassName('burgerDiv');
 var burgerMenuOverlayWhite = document.getElementsByClassName('burgerMenuOverlayWhite')[0];
 var burgerOverlayBlackBox = document.getElementsByClassName('burgerOverlayBlackBox')[0];
 var wholePageOverlay = document.getElementsByClassName('wholePageOverlay')[0];
 var closeBox = document.getElementsByClassName('closeBox')[0];
-burgerDiv.addEventListener('click',function(){
-  burgerMenuOverlayWhite.style.width = "100%"
+var stickySwitch = 0;
+burgerDiv[0].addEventListener('click',function(){
+  stickySwitch =1;
+  burgerMenuOverlayWhite.style.width = "100%";
+  stickyMenu.style.height = '0vw';
   setTimeout(function(){
-    burgerOverlayBlackBox.style.width = '20vw'
-    wholePageOverlay.style.filter = 'blur(10px)'
+    burgerOverlayBlackBox.style.width = '20vw';
+    wholePageOverlay.style.filter = 'blur(10px)';
+  },1000)
+})
+burgerDiv[1].addEventListener('click',function(){
+  stickySwitch =1;
+  burgerMenuOverlayWhite.style.width = "100%";
+  stickyMenu.style.height = '0vw';
+  setTimeout(function(){
+    burgerOverlayBlackBox.style.width = '20vw';
+    wholePageOverlay.style.filter = 'blur(10px)';
   },1000)
 })
 
 closeBox.addEventListener('click',function(){
+  stickySwitch = 0;
   wholePageOverlay.style.filter = 'blur(0px)'
   setTimeout(function(){
     burgerMenuOverlayWhite.style.width = "0%"
@@ -73,7 +86,8 @@ var yellowBoxBar = document.getElementsByClassName('yellowBoxBar')[0];
 var getinTouchImageBox = document.getElementsByClassName('getinTouchImageBox')[0];
 var getInTouchYellowBox = document.getElementsByClassName('getInTouchYellowBox')[0];
 
-window.onscroll = function(){
+//WINDOW ONSCROLL FUNCTION CALLLL
+window.onscroll = function(e){
     if(main.getBoundingClientRect().top - window.innerHeight <= 0){
       yellowBoxBar.style.backgroundColor = '#f0cd3c';
     }
@@ -81,8 +95,15 @@ window.onscroll = function(){
     if(getinTouchImageBox.getBoundingClientRect().top - (window.innerHeight/1.5)<= 0){
       getInTouchYellowBox.style.backgroundColor = 'rgb(217,185,86)';
     }
+
     threeGlasAnim();
     specialsAnim();
+    yellowBoxAnim();
+    fromTheSectionAnim();
+    kidsMenuAnim();
+    ourMenuAnim();
+    cateringAnim();
+    stickyMenuFunc();
 }
 //3 pic glass animation
 var threePicDiv = document.getElementsByClassName('threePicDiv')[0];
@@ -92,6 +113,13 @@ function threeGlasAnim(){
     threePicDiv.style.bottom = '0'
   }
 }
+//Bolero first banner paragraph animation
+var grillTextAnimOneParagraph = document.getElementsByClassName('grillTextAnimOneParagraph')[0];
+var grillTextAnimTwoParagraph = document.getElementsByClassName('grillTextAnimTwoParagraph')[0];
+function bannerOneParagraph(){
+  grillTextAnimOneParagraph.style.top = '0';
+  grillTextAnimTwoParagraph.style.top = '0';
+}
 //specials reveal anim specials connectWithUsParagraph
 var specials = document.getElementsByClassName('specials')[0];
 var connectWithUsParagraph = document.getElementsByClassName('connectWithUsParagraph')[0];
@@ -100,4 +128,74 @@ function specialsAnim(){
     specials.style.backgroundPosition = '0 0';
     connectWithUsParagraph.style.top = '0';
   }
+}
+//yellow box heading animation paragraph
+var yellowParaAnimOne = document.getElementsByClassName('yellowParaAnimOne')[0];
+var yellowParaAnimTwo = document.getElementsByClassName('yellowParaAnimTwo')[0];
+function yellowBoxAnim(){
+  if(yellowParaAnimOne.getBoundingClientRect().top - (window.innerHeight/1.1)<= 0){
+    yellowParaAnimOne.style.top = '0';
+    yellowParaAnimTwo.style.top = '0';
+  }
+}
+//from THE section
+var fromTheSectionParagAnim = document.getElementsByClassName('fromTheSectionParagAnim')[0];
+var barPic = document.getElementsByClassName('barPic')[0];
+function fromTheSectionAnim(){
+  if(barPic.getBoundingClientRect().top - (window.innerHeight/1.2)<= 0){
+    fromTheSectionParagAnim.style.top = '0';
+    barPic.style.backgroundPosition = '0 0';
+  }
+}
+//OUR KIDS section imageSection
+var imageSectionParagAnim = document.getElementsByClassName('imageSectionParagAnim')[0];
+var menuPng = document.getElementsByClassName('menuPng')[0];
+function kidsMenuAnim(){
+  if(menuPng.getBoundingClientRect().top - (window.innerHeight/1.2)<= 0){
+    imageSectionParagAnim.style.top = '0';
+    menuPng.style.backgroundPosition = '0 0';
+  }
+}
+//OUR MENU sectionImageTwo
+var imageSectionTwoParagAnim = document.getElementsByClassName('imageSectionTwoParagAnim')[0];
+var blackBandMenuText = document.getElementsByClassName('blackBandMenuText')[0];
+function ourMenuAnim(){
+  if(blackBandMenuText.getBoundingClientRect().top - (window.innerHeight/1.2)<= 0){
+    imageSectionTwoParagAnim.style.top = '0';
+    blackBandMenuText.style.backgroundPosition = '0 0';
+  }
+}
+//CATTERING SECTION
+var cateringParagAnim = document.getElementsByClassName('cateringParagAnim')[0];
+var events = document.getElementsByClassName('events')[0];
+function cateringAnim(){
+  if(events.getBoundingClientRect().top - (window.innerHeight/1.2)<= 0){
+    cateringParagAnim.style.top = '0';
+    events.style.backgroundPosition = '0 0';
+  }
+}
+//Animate his lemons and mushrooms
+var lemon = document.getElementsByClassName('lemon')[0];
+var mushroom = document.getElementsByClassName('mushroom')[0];
+function cateringAnim(){
+  if(mushroom.getBoundingClientRect().top - (window.innerHeight/1.2)<= 0){
+    lemon.style.left = '0';
+    mushroom.style.right = '16.592vw'
+  }
+}
+//STYCKY Menu
+var stickyMenu = document.getElementsByClassName('stickyMenu')[0];
+function stickyMenuFunc(){
+  if(this.oldScroll > this.scrollY && window.scrollY > 1 && stickySwitch===0){
+    stickyMenu.style.height = '7vw';
+  }
+  else {
+    stickyMenu.style.height = '0vw';
+  }
+  this.oldScroll = this.scrollY;
+}
+
+//window onload
+window.onload = function(){
+  bannerOneParagraph();
 }
